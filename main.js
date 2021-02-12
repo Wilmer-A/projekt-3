567d4a18a7967939dd8065957fe372778a70b8ed
Wilmer Andersson <wilmer.andersson2@elev.ga.ntig.se>
2021-02-12 15:00:54 +0100

    Fortsatt övning av java, funktioner

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 let listItems = ["Sak nr 1", "Sak nr 2"]
 
-let listInput = document.querySelector(".list-input");
-let listForm = document.querySelector(".list-form");
-let listContainer = document.querySelector(".list-container");
+let listInput = document.querySelector("[list-input]");
+let listForm = document.querySelector("[list-form]");
+let listContainer = document.querySelector("#list-container");
 
+listForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (listInput.value.trim() == "") {
+        return;
+    }
+    listItems.push(listInput.value.trim())
+    listInput.value = "";
+    updateList();
+})
+
+function createList(listItems) {
+    let list = document.createElement("ul");
+    listItems.forEach((item) => {
+        let newListItem = document.createElement("li")
+        newListItem.innerText = item;
+        newListItem.classList.add("todo");
+        list.append(newListItem);
+    })
+    return list;
+}
+
+function updateList() {
+    listContainer.innerHTML = "";
+    listContainer.append(createList(listItems));
+}
+
+updateList();
