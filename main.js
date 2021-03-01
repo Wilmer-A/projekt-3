4372b8847326cdcdb3b7883f073bc75c499d62fc
Wilmer Andersson <wilmer.andersson2@elev.ga.ntig.se>
2021-03-01 18:29:24 +0100

    ta bort-knapp + klar-knapp

--- main.js
+++ main.js
@@ -1,28 +1,60 @@
-let listItems = ["Använd text-rutan och tryck på plusset för att skriva din lista", "(lägg till info om ta-bort-funktion)"]
-
+let listItems = ["Gör klart webbsidan"]
 let listInput = document.querySelector("[list-input]");
 let listForm = document.querySelector("[list-form]");
 let listContainer = document.querySelector("#list-container");
 
 listForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (listInput.value.trim() == "") {
+    if (listInput.value.trim() === "") {
         return;
     }
-    listItems.push(listInput.value.trim())
-    listInput.value = "";
+    listItems.push(listInput.value.trim());
     updateList();
-})
+    listInput.value = "";
+});
 
-function createList(listItems) {
+function createList(items) {
     let list = document.createElement("ul");
-    listItems.forEach((item) => {
-        let newListItem = document.createElement("li")
+    items.forEach((item) => {
+        let newListItem = document.createElement("li");
         newListItem.innerText = item;
-        newListItem.classList.add("todo");
+        newListItem.classList.add("todo-text");
+        newListItem.addEventListener("click", removeItem);
+        newListItem.addEventListener("click", checkItem);
         list.append(newListItem);
-    })
+
+        let checkmarkButton = document.createElement("button");
+        checkmarkButton.innerHTML = "✓";
+        checkmarkButton.classList.add("complete-button");
+        newListItem.append(checkmarkButton);
+
+        let xButton = document.createElement("button");
+        xButton.innerHTML = "✗";
+        xButton.classList.add("remove-button");
+        newListItem.append(xButton);
+    });
     return list;
+}
+
+function removeItem(event) {
+    let itemToRemove = event.target;
+    if (itemToRemove.classList.contains("remove-button")){
+        let textToRemove = itemToRemove.parentElement;
+        textToRemove.remove();
+    }
+}
+
+function checkItem(event) {
+    let itemToCheck = event.target;
+    if (itemToCheck.classList.contains("complete-button")){
+        let textToCheck = itemToCheck.parentElement;
+        textToCheck.classList.toggle("completed-item");
+    }
+}
+
+function markItem(event) {
+    let itemToMark = event.target;
+    
 }
 
 function updateList() {
